a3a21992016cbf6345f9fccbd707ac53bb6dd588
Stefan <stefanboblic@gmail.com>
2021-09-11 16:34:15 +0300

    created project

---
+++ src/main/java/md/maib/app/maib/MaibApplication.java
@@ -0,0 +1,13 @@
+package md.maib.app.maib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaibApplication.class, args);
+    }
+
+}
---
+++ src/test/java/md/maib/app/maib/MaibApplicationTests.java
@@ -0,0 +1,13 @@
+package md.maib.app.maib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaibApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
