44834ca8bb9126f515b44c1d51ed5379c402cd51
Stefan <stefanboblic@gmail.com>
2021-09-11 18:33:40 +0300

    added app properties for db connection and created client class

---
+++ src/main/java/md/maib/app/maib/entity/Client.java
@@ -0,0 +1,71 @@
+package md.maib.app.maib.entity;
+
+import javax.persistence.*;
+import java.util.Date;
+
+
+@Entity
+@Table(name = "Sciot")
+public class Client {
+
+    private int id;
+    private int number;
+    private int balance;
+    private String currency;
+    private Date last_oper_time;
+
+    public Client() {
+
+    }
+
+    public Client(int id, int number, int balance, String currency, Date last_oper_time) {
+        this.id = id;
+        this.number = number;
+        this.balance = balance;
+        this.currency = currency;
+        this.last_oper_time = last_oper_time;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "Number", nullable = false)
+    public int getNumber() {
+        return number;
+    }
+    public void setNumber(int number) {
+        this.number = number;
+    }
+    @Column(name = "Balance", nullable = false)
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    @Column(name = "Currnecy", nullable = false)
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    @Column(name = "LastOperationTime", nullable = false)
+    public Date getLast_oper_time() {
+        return last_oper_time;
+    }
+
+    public void setLast_oper_time(Date last_oper_time) {
+        this.last_oper_time = last_oper_time;
+    }
+}
